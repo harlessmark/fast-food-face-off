b64a98f73b629b2edf5360be723b9e26970f2707
superhackerboy <mark@superhackerman.com>
2020-01-08 14:08:03 -0500

    Finished setState logic.

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -4,51 +4,26 @@ import Foods from "../foods.json";
 class Game extends Component {
   state = {
     firstFood: Foods[Math.floor(Math.random() * Foods.length)],
-    secondFood: Foods[Math.floor(Math.random() * Foods.length)],
+    secondFood: {},
     mostCalories: "placeholder"
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      // checks to see two foods' calories aren't the same
-      while (
-        this.state.firstFood.attributes.calories ===
-        this.state.secondFood.attributes.calories
-      ) {
-        console.log(this.state.firstFood.attributes.calories);
-        console.log(this.state.secondFood.attributes.calories);
-        debugger;
-
-        this.setState({
-          firstFood: Foods[Math.floor(Math.random() * Foods.length)]
-        });
-      }
-    }, 500);
-
-    if (
-      this.state.firstFood.attributes.calories >
-      this.state.secondFood.attributes.calories
-    ) {
-      this.setState({
-        mostCalories: this.state.firstFood
-      });
-    }
-
-    if (
-      this.state.firstFood.attributes.calories >
-      this.state.secondFood.attributes.calories
-    ) {
-      this.setState({
-        mostCalories: this.state.firstFood
-      });
-    } else {
-      this.setState({
-        mostCalories: this.state.secondFood
-      });
-    }
+    this.setState(prevState => {
+      const filteredList = Foods.filter(
+        foodItem =>
+          foodItem.attributes.calories !==
+          prevState.firstFood.attributes.calories
+      );
+      return {
+        secondFood:
+          filteredList[Math.floor(Math.random() * filteredList.length)]
+      };
+    });
   }
 
   render() {
+    console.log(this.state);
     return <></>;
   }
 }
@@ -71,3 +46,41 @@ export default Game;
 //     alt="restaurant logo"
 //   />
 // </div>
+
+// setTimeout(() => {
+//   // checks to see two foods' calories aren't the same
+//   while (
+//     this.state.firstFood.attributes.calories ===
+//     this.state.secondFood.attributes.calories
+//   ) {
+//     console.log(this.state.firstFood.attributes.calories);
+//     console.log(this.state.secondFood.attributes.calories);
+//     debugger;
+//
+//     this.setState({
+//       firstFood: Foods[Math.floor(Math.random() * Foods.length)]
+//     });
+//   }
+// }, 500);
+//
+// if (
+//   this.state.firstFood.attributes.calories >
+//   this.state.secondFood.attributes.calories
+// ) {
+//   this.setState({
+//     mostCalories: this.state.firstFood
+//   });
+// }
+//
+// if (
+//   this.state.firstFood.attributes.calories >
+//   this.state.secondFood.attributes.calories
+// ) {
+//   this.setState({
+//     mostCalories: this.state.firstFood
+//   });
+// } else {
+//   this.setState({
+//     mostCalories: this.state.secondFood
+//   });
+// }
