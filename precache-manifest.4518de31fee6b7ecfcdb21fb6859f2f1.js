617b60398280dd98eadc460779fc8cd5c6f2d5bb
superhackerboy <mark@superhackerman.com>
2020-02-24 16:40:15 -0500

    Updates

--- precache-manifest.4518de31fee6b7ecfcdb21fb6859f2f1.js
+++ precache-manifest.4518de31fee6b7ecfcdb21fb6859f2f1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3467e04aeb1f7b926c4eb3284d037ec3",
+    "revision": "cc622becbf7cf26cf7e77e4688db2e07",
     "url": "/fast-food-face-off/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fast-food-face-off/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "85aaa1c71cc28b1996cd",
+    "revision": "1eb1b6776e1dc4b1811f",
     "url": "/fast-food-face-off/static/css/main.e6577679.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fast-food-face-off/static/js/2.4b3719af.chunk.js.LICENSE"
   },
   {
-    "revision": "85aaa1c71cc28b1996cd",
-    "url": "/fast-food-face-off/static/js/main.f0344990.chunk.js"
+    "revision": "1eb1b6776e1dc4b1811f",
+    "url": "/fast-food-face-off/static/js/main.24ff2fa3.chunk.js"
   },
   {
     "revision": "0758d2df09f76948df50",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fast-food-face-off/precache-manifest.ca97b3b2e81cc7d3c87cf5feaa0d4f40.js"
+  "/fast-food-face-off/precache-manifest.4518de31fee6b7ecfcdb21fb6859f2f1.js"
 );
 
 self.addEventListener('message', (event) => {
