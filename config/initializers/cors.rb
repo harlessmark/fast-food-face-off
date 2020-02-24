8190b8bd3b385c7b492a56e9197f242657ef5b10
superhackerboy <mark@superhackerman.com>
2020-02-24 14:34:13 -0500

    Updated CORS policy.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://superhackerboy.github.io/fast-food-face-off/'
+    origins 'https://superhackerboy.github.io'
 
     resource '*',
       headers: :any,
